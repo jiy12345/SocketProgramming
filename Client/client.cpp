57ba7ed7c3bc4bade8ac88bb09f3784f48c93aa8
DESKTOP-1GGFRU5\jiy12345 <jiy12367@gmail.com>
2023-01-05 18:04:35 +0900

    feat: Implement a basic one-to-one echo server

---
+++ Client/client.cpp
@@ -0,0 +1,78 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include<iostream>
+#include<winsock2.h>
+
+int main() {
+	WSAData wsa_data;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+		return -1;
+	}
+
+	SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// 서버의 소켓 주소 설정
+	sockaddr_in socket_address;
+	socket_address.sin_family = AF_INET;
+	socket_address.sin_addr.s_addr = inet_addr("192.168.0.174");
+	socket_address.sin_port = htons(10000);
+	int return_value = connect(client_socket, (sockaddr*)&socket_address, sizeof(socket_address));
+	if (return_value == SOCKET_ERROR) {
+		int error_num = WSAGetLastError();
+		printf("%d ", error_num);
+		closesocket(client_socket);
+		WSACleanup();
+		return error_num;
+	}
+
+	// 논 블로킹 소켓으로 변경
+	u_long mode = TRUE;
+	ioctlsocket(client_socket, FIONBIO, &mode);
+
+	while (true) {
+		// 서버로 데이터 보내기
+		char msg_to_send[256] = { 0, };
+		printf("보낼 데이터: ");
+		fgets(msg_to_send, 256, stdin);
+		int sended_bytes = send(client_socket, msg_to_send, strlen(msg_to_send), 0);
+		// 논 블로킹 소켓이므로 아직 보내지 못했을 때도 SOCKET_ERROR 반환
+		if (sended_bytes == SOCKET_ERROR) {
+			// 아직 보내지 못했을 때는 항상 에러가 WSAEWOULDBLOCK 값이어야 한다!
+			// 그렇지 않으면 에러
+			if (WSAGetLastError() != WSAEWOULDBLOCK)
+			{
+				int error_num = WSAGetLastError();
+				printf("%d ", error_num);
+				closesocket(client_socket);
+				WSACleanup();
+				return error_num;
+
+			}
+		}
+
+		char msg_to_recv[256] = { 0, };
+		int recved_bytes = recv(client_socket, msg_to_recv, 256, 0);
+		if (recved_bytes == SOCKET_ERROR) {
+			// 아직 받지 못했을 때는 항상 에러가 WSAEWOULDBLOCK 값이어야 한다!
+			// 그렇지 않으면 에러
+			if (WSAGetLastError() != WSAEWOULDBLOCK)
+			{
+				int error_num = WSAGetLastError();
+				printf("%d ", error_num);
+				closesocket(client_socket);
+				WSACleanup();
+				return error_num;
+			}
+		}
+		else if (recved_bytes == 0) { // 연결이 닫혔을 경우
+			closesocket(client_socket);
+			WSACleanup();
+			return 0;
+		}
+		else {
+			printf("받은 데이터: %s", msg_to_recv);
+		}
+	}
+
+	closesocket(client_socket);
+	WSACleanup();
+}
---
+++ Server/server.cpp
@@ -0,0 +1,80 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include<iostream>
+#include<winsock2.h>
+
+int main() {
+	WSAData wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		return -1;
+	}
+
+	// 연결되어야 하는 통신 방법이 정해져 있으므로, 0으로 적어 기본 값을 설정하도록 하면된다!
+	SOCKET listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// 서버측의 소켓 생성
+	// 어떤 주소에서든 접근할 수 있도록 모든 주소와 연결 가능하도록 설정
+	SOCKADDR_IN sa;
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = htonl(INADDR_ANY);
+	sa.sin_port = htons(10000);
+	int return_value = bind(listen_socket, (sockaddr*)&sa, sizeof(sa));
+	if (return_value == SOCKET_ERROR)
+	{
+		int error_num = WSAGetLastError();
+		printf("%d ", error_num);
+		closesocket(listen_socket);
+		WSACleanup();
+		return error_num;
+	}
+	return_value = listen(listen_socket, SOMAXCONN);
+	if (return_value == SOCKET_ERROR)
+	{
+		int error_num = WSAGetLastError();
+		printf("%d ", error_num);
+		closesocket(listen_socket);
+		WSACleanup();
+		return error_num;
+	}
+
+	// 접속을 시도하는 클라이언트 받아들이기
+	SOCKADDR_IN clientaddr;
+	int length = sizeof(clientaddr);
+	SOCKET client_socket = accept(listen_socket, (sockaddr*)&clientaddr, &length);
+	printf("클라이언트 접속 : IP:%s, PORT:%d\n",
+		inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+	while (true) {
+		char msg_to_recv[256] = { 0, };
+		int recved_bytes = recv(client_socket, msg_to_recv, 256, 0);
+		if (recved_bytes == SOCKET_ERROR) {
+			int error_num = WSAGetLastError();
+			printf("%d ", error_num);
+			closesocket(client_socket);
+			closesocket(listen_socket);
+			WSACleanup();
+		}
+		printf("%s\n", msg_to_recv);
+
+		int sended_bytes = send(client_socket, msg_to_recv, strlen(msg_to_recv), 0);
+		// 논 블로킹 소켓이므로 아직 보내지 못했을 때도 SOCKET_ERROR 반환
+		if (sended_bytes == SOCKET_ERROR) {
+			// 아직 보내지 못했을 때는 항상 에러가 WSAEWOULDBLOCK 값이어야 한다!
+			// 그렇지 않으면 에러
+			if (WSAGetLastError() != WSAEWOULDBLOCK)
+			{
+				int error_num = WSAGetLastError();
+				printf("%d ", error_num);
+				closesocket(client_socket);
+				closesocket(listen_socket);
+				WSACleanup();
+				return error_num;
+			}
+		}
+
+	}
+
+
+	closesocket(listen_socket);
+
+	WSACleanup();
+}
