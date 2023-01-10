06120c0d2793037fbd71b67fb9a2d4114a3d381d
DESKTOP-1GGFRU5\jiy12345 <jiy12367@gmail.com>
2023-01-10 15:05:53 +0900

    feat: Write a protocol
    - Write a protocol that both clients and servers will use of the server

---
+++ Client/Protocol.cpp
@@ -0,0 +1,30 @@
+#pragma once
+#define PACKET_HEADER_SIZE 4
+#pragma pack(push, 1)
+typedef struct
+{
+	short len;
+	short type;
+}PACKET_HEADER;
+
+typedef struct
+{
+	PACKET_HEADER ph;
+	char   msg[2048];
+}PACKET_DATA;
+
+typedef struct {
+	char    szName[9];
+}USERNAME_REQ;
+
+typedef struct {
+	int		iResult;
+}USERNAME_ACK;
+#pragma pack(pop)
+
+#define PACKET_CHAR_MSG   1000      // client ->
+#define PACKET_CHATNAME_REQ   1001  // server -> client
+#define PACKET_NAME_REQ   2000		// client -> server
+#define PACKET_NAME_ACK   3000		// server -> client
+#define PACKET_JOIN_USER  4000		// server -> client
+
